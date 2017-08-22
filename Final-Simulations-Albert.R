ee494806bb3a528aa8a4e6aa4bd8d1b644118c56
Albert Y. Kim <albert.ys.kim@gmail.com>
2017-08-22 13:44:26 -0400

    Updated code

--- Final-Simulations-Albert.R
+++ Final-Simulations-Albert.R
@@ -2,7 +2,6 @@
 
 # Setup -------------------------------------------------------------------
 # setwd("~/Documents/Stats Indep Study/Simulations")
-setwd("~/Desktop/")
 
 # tidyverse includes ggplot2, tibble, tidyr, readr, purrr, and dplyr:
 library(tidyverse)
@@ -20,14 +19,17 @@ B_values <- seq(from = .01, to = 1, by = .01)
 covariates <- data_frame(
   A = rep(A_values, times=length(B_values)),
   B = rep(B_values, each=length(A_values))
-)
+) %>% 
+  mutate(ID=1:n()) %>% 
+  dplyr::select(ID, A, B)
 
 # Number of simulations
-n_sim <- 100
+n_sim <- 10
 
 # Names of estimation models
-model_names <- c("1_0", "1_1", "1_2", "2_1", "2_2", "3_1", "3_2") %>% 
-  str_c("model_", .)
+model_names <- c("1_0", "1_1", "1_2", "2_1", "2_2", "3_1", "3_2")
+
+
 
 
 
@@ -95,9 +97,12 @@ ggplot(P_1, aes(x=A, y=B)) +
 
 
 
+
+
 # Main Simulation Loop ----------------------------------------------------
-str_c("Simulations started at", Sys.time(), sep=" ") %>% 
-  print()
+# Save results here
+simulation_results <- NULL
+saved_P <- NULL
 
 for(i in 1:2){
   # Set propensity scores, potential outcomes & delta based on simulation number
@@ -106,9 +111,6 @@ for(i in 1:2){
   } else if (i == 2) {
     P <- P_2
   }
-  
-  # Save results here
-  simulation_results <- NULL
   
   # Main for loop
   for (k in 1:n_sim) {
@@ -119,6 +121,15 @@ for(i in 1:2){
         di = rbinom(n = n(), size = 1, prob = prop_score),
         yi = ifelse(di == 1, y1, y0)
       )
+    # Save treated/untreated variable and resulting observation 
+    saved_P <- P %>% 
+      mutate(
+        simulation = i,
+        iteration = k
+      ) %>% 
+      # dplyr::select(simulation, iteration, ID, di) %>% 
+      dplyr::select(di) %>% 
+      bind_rows(saved_P, .)
     
     # Estimation 1.0
     model_1_0 <- P %>% 
@@ -158,7 +169,7 @@ for(i in 1:2){
       predict(type = "response") %>% 
       tbl_df() %>% 
       rename(pi = value) %>% 
-      bind_cols(P, .) %>% 
+      bind_cols(P) %>% 
       # Compute weights
       mutate(wi = di/pi + (1-di)/(1-pi)) %>% 
       lm(yi ~ di + B, weights = wi, data = .) %>% 
@@ -171,19 +182,23 @@ for(i in 1:2){
       predict(type = "response") %>% 
       tbl_df() %>% 
       rename(pi = value) %>% 
-      bind_cols(P, .) %>% 
+      bind_cols(P) %>% 
       # Compute weights
       mutate(wi = di/pi + (1-di)/(1-pi)) %>% 
       lm(yi ~ di + A + B, weights = wi, data = .) %>% 
       tidy()
     
-    # Save results for this simulation k
+    # Save simulation results for this k
     ID_variables <- data_frame(
-      model = model_names,
-      iteration = k
+      simulation = i,
+      iteration = k,
+      model = model_names
     )
     beta_di <- 
-      bind_rows(model_1_0, model_1_1, model_1_2, model_2_1, model_2_2, model_3_1, model_3_2) %>% 
+      bind_rows(
+        model_1_0, model_1_1, model_1_2, 
+        model_2_1, model_2_2, model_3_1, model_3_2
+      ) %>% 
       filter(term == "di") %>% 
       dplyr::select(estimate, std.error)
     
@@ -191,20 +206,70 @@ for(i in 1:2){
     simulation_results <- simulation_results %>% 
       bind_rows(bind_cols(ID_variables, beta_di))
     
-    # Show simulation counter
-    if(k %% 10 == 0)
-      str_c("Simulation", i, "Iteration", k, "of", n_sim, "done at", Sys.time(), sep=" ") %>% 
-      print()
-  }
-  
-  # Assing simulation results depending on simulation number
-  if(i == 1){
-    simulation_results_1 <- simulation_results
-  } else if (i == 2) {
-    simulation_results_2 <- simulation_results
+    # Show simulation counter and save results
+    if(k %% 10 == 1){
+      str_c("Sim", i, "Iter", k, "of", n_sim, "done at", Sys.time(), sep=" ") %>%
+        print()
+      save(simulation_results, file="simulation_results.RData")
+      save(saved_P, file="saved_P.RData")
+    }
   }
 }
+save(simulation_results, file="simulation_results.RData")
+save(saved_P, file="saved_P.RData")
 
-save(simulation_results_1, simulation_results_2, file="simulation_results.RData")
+
+
+
+load("simulation_results.RData")
+
+ATE <- bind_rows(
+  P_1 %>%
+    summarise(ATE = mean(delta)) %>% 
+    mutate(simulation = 1),
+  P_2 %>%
+    summarise(ATE = mean(delta)) %>% 
+    mutate(simulation = 2)
+)
+
+simulation_results_1 <- simulation_results_1 %>% 
+  mutate(simulation = 1)
+simulation_results_2 <- simulation_results_2 %>% 
+  mutate(simulation = 2)
+
+simulation_results_2 %>% 
+  #bind_rows(simulation_results_2) %>% 
+  mutate(
+    model = str_sub(model, start=7),
+    method = str_sub(model, start=1, end=1),
+    method = as.factor(method)
+    ) %>% 
+  left_join(ATE, by="simulation") %>% 
+  mutate(bias = estimate - ATE) %>% 
+  dplyr::select(model, method, simulation, bias, std.error) %>% 
+  gather(type, value, -c(model, method, simulation)) %>% 
+  ggplot() +
+  geom_boxplot(aes(x=model, y=value, fill=method)) +
+  facet_wrap(~type, scales = "free") +
+  labs(title = "Simulation 2", y = "") +
+  scale_fill_brewer(palette = "Set2")
+  # facet_wrap(~simulation) + 
+  # geom_hline(yintercept = 0, linetype = "dashed") +
+
+
+simulation_results %>% 
+  ggplot() +
+  geom_boxplot(aes(x=model, y=bias)) +
+  # facet_wrap(~simulation) + 
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  labs(title="Bias of Estimator")
+
+simulation_results %>% 
+  ggplot() +
+  geom_boxplot(aes(x=model, y=std.error)) +
+  # facet_wrap(~simulation) +
+  labs(title="Standard Error of Estimator")
+
+
 
 
