83a9ef06110e5e027a99d20c6cc6e7cf3c45adb9
rudeboybert <albert.ys.kim@gmail.com>
2018-01-18 15:39:43 -0500

    Added first pass at figures code

---
+++ figures.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(stringr)
+results <- readxl::read_xlsx("Placeholder_Results.xlsx") %>%
+  mutate(`Method (line)` = str_sub(`Method (line)`, 7, 7))
+
+# Two options for faceting
+baseplot_1 <- ggplot(results, aes(x = `Strength of confounding (x axis)`, col = `Method (line)`, shape = `Method (line)`)) +
+  facet_wrap(~`PropHTE (facet)`, nrow = 2) +
+  labs(x = "Strength of Confounding", col = "Method")
+
+baseplot_2 <- ggplot(results, aes(x = `Strength of confounding (x axis)`, col = `Method (line)`, shape = `Method (line)`)) +
+  facet_grid(HTE ~ PropModel) +
+  labs(x = "Strength of Confounding", col = "Method")
+
+
+# Bias
+baseplot_1 +
+  geom_line(aes(y = `Bias (y axis)`)) +
+  geom_point(aes(y = `Bias (y axis)`), show.legend = F) +
+  labs(y = "Bias", title = "Bias")
+ggsave("figures/bias_1.png", width=8, height=4.5)
+
+baseplot_2 +
+  geom_line(aes(y = `Bias (y axis)`)) +
+  geom_point(aes(y = `Bias (y axis)`), show.legend = F) +
+  labs(y = "Bias", title = "Bias")
+ggsave("figures/bias_2.png", width=8, height=4.5)
+
+
+# Variance
+baseplot_1 +
+  geom_line(aes(y = `Variance (y axis)`)) +
+  geom_point(aes(y = `Variance (y axis)`), show.legend = F) +
+  labs(y = "Variance", title = "Variance")
+ggsave("figures/variance_1.png", width=8, height=4.5)
+
+baseplot_2 +
+  geom_line(aes(y = `Variance (y axis)`)) +
+  geom_point(aes(y = `Variance (y axis)`), show.legend = F) +
+  labs(y = "Variance", title = "Variance")
+ggsave("figures/variance_2.png", width=8, height=4.5)
+
+
