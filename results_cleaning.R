57e2edd95a676a62106bc27c14c65f34d92f73c8
trishasingh <tsingh@middlebury.edu>
2018-02-27 02:58:37 -0500

    Added metrics and generated figures

--- results_cleaning.R
+++ results_cleaning.R
@@ -1,26 +1,35 @@
 library(tidyverse)
 library(stringr)
-results <- read_csv("results_for_figure_0224.csv") %>%
+results <- read_csv("results_for_figure.csv") %>%
   mutate(`Method (line)` = str_sub(`Method (line)`, 7, 7)) %>% 
   group_by(`PropHTE (facet)`, `Strength of confounding (x axis)`, `Method (line)`, PropModel, HTE) %>% 
-  summarize(`Bias (y axis)` = mean(`Bias (y axis)`), `Variance (y axis)` = mean(`Variance (y axis)`))
+  summarize(`Bias (y axis)` = mean(`Bias (y axis)`), `SE (y axis)` = mean(`SE (y axis)`),
+            `RMSE (y axis)` = sqrt(mean(`RMSE (y axis)`)))
 
-baseplot_1 <- ggplot(results, aes(x = `Strength of confounding (x axis)`, col = `Method (line)`, shape = `Method (line)`)) +
-  facet_wrap(~`PropHTE (facet)`, nrow = 2) +
-  labs(x = "Strength of Confounding", col = "Method")
+# Relative Bias Plot
 
-baseplot_1 +
-  geom_line(aes(y = `Bias (y axis)`)) +
-  geom_point(aes(y = `Bias (y axis)`), show.legend = F) +
-  labs(y = "Bias", title = "Bias")
-ggsave("figures/bias_1.png", width=8, height=4.5)
-
-baseplot_2 <- ggplot(results, aes(x = `Strength of confounding (x axis)`, col = `Method (line)`, shape = `Method (line)`)) +
+baseplot <- ggplot(results, aes(x = `Strength of confounding (x axis)`, col = `Method (line)`, shape = `Method (line)`)) +
   facet_grid(HTE ~ PropModel) +
   labs(x = "Strength of Confounding", col = "Method")
 
-baseplot_2 +
+baseplot +
   geom_line(aes(y = `Bias (y axis)`)) +
   geom_point(aes(y = `Bias (y axis)`), show.legend = F) +
   labs(y = "Bias", title = "Bias")
-ggsave("figures/bias_2.png", width=8, height=4.5)
+ggsave("figures/bias.png", width=8, height=4.5)
+
+# SE Plot
+
+baseplot +
+  geom_line(aes(y = `SE (y axis)`)) +
+  geom_point(aes(y = `SE (y axis)`), show.legend = F) +
+  labs(y = "Standard Error", title = "Standard Error")
+ggsave("figures/se.png", width=8, height=4.5)
+
+# RMSE Plot
+
+baseplot +
+  geom_line(aes(y = `RMSE (y axis)`)) +
+  geom_point(aes(y = `RMSE (y axis)`), show.legend = F) +
+  labs(y = "RMSE", title = "RMSE")
+ggsave("figures/rmse.png", width=8, height=4.5)
