82fbd32c95445a796f14bdfe07aa98cbb9b1327e
trishasingh <tsingh@middlebury.edu>
2018-02-27 16:05:42 -0500

    New script for sims.

--- results_cleaning.R
+++ results_cleaning.R
@@ -1,5 +1,8 @@
+# Setup
 library(tidyverse)
 library(stringr)
+setwd("~/Dropbox/Matching Paper/Matching Git")
+
 results <- read_csv("results_for_figure.csv") %>%
   mutate(`Method (line)` = str_sub(`Method (line)`, 7, 7)) %>% 
   group_by(`PropHTE (facet)`, `Strength of confounding (x axis)`, `Method (line)`, PropModel, HTE) %>% 
